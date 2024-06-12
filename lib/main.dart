7afa895365a435a5cefd1b13260e93c81d141993
Sarangthem Amarjit Meetei <sarangthemamarjit123@gmail.com>
2024-06-12 08:57:39 +0530

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const HomePage(),
+        theme: ThemeData(
+          fontFamily: 'KulimPark',
+          useMaterial3: false,
+        ));
   }
 }
--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -5,146 +5,185 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(50),
-          child: Card(
-            elevation: 10,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                gradient: const LinearGradient(
-                  colors: [Color(0xFFE0E0E0), Color(0xFFF5F5F5)],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                ),
-              ),
-              child: LayoutBuilder(
-                builder: (context, constraints) {
-                  bool isSmallScreen = constraints.maxWidth < 800;
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 30, vertical: 30),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        // Header Row
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return DefaultTabController(
+      length: 4, // Number of tabs
+      child: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 219, 226, 225),
+                Color.fromARGB(255, 232, 227, 227),
+                Color(0xFFF5F5F5),
+                Color.fromARGB(255, 227, 216, 204),
+                Color.fromARGB(255, 201, 190, 189)
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(50),
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                  ),
+                  child: LayoutBuilder(
+                    builder: (context, constraints) {
+                      bool isSmallScreen = constraints.maxWidth < 800;
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 30, vertical: 30),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
                           children: [
-                            const Text(
-                              'My Portfolio',
-                              style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            if (!isSmallScreen) ...[
-                              Row(
-                                children: [
-                                  TextButton(
-                                    onPressed: () {},
-                                    child: const Text(
-                                      'About',
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                      ),
-                                    ),
+                            // Header Row with TabBar
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text(
+                                  'My Portfolio',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
                                   ),
-                                  TextButton(
-                                    onPressed: () {},
-                                    child: const Text(
-                                      'Services',
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                  ),
-                                  TextButton(
-                                    onPressed: () {},
-                                    child: const Text(
-                                      'My work',
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                  ),
-                                  TextButton(
-                                    onPressed: () {},
-                                    child: const Text(
-                                      'Blog',
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(width: 10),
-                                  ElevatedButton(
-                                    onPressed: () {},
-                                    style: ElevatedButton.styleFrom(
-                                      backgroundColor: Colors.white,
-                                      foregroundColor: Colors.black,
-                                      shape: const StadiumBorder(),
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 20, vertical: 10),
-                                    ),
-                                    child: const Text('Hire Me'),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ],
-                        ),
-                        const SizedBox(height: 40),
-                        Expanded(
-                          child: isSmallScreen
-                              ? SingleChildScrollView(
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
+                                ),
+                                if (!isSmallScreen)
+                                  Row(
                                     children: [
-                                      // Developer Image
-                                      Image.asset(
-                                        'assets/images/developer.png',
-                                        height: 250,
+                                      const TabBar(
+                                        labelStyle: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18,
+                                        ),
+                                        dividerHeight: 100,
+                                        dividerColor: Colors.transparent,
+                                        automaticIndicatorColorAdjustment: true,
+                                        isScrollable: true,
+                                        unselectedLabelStyle:
+                                            TextStyle(fontSize: 15),
+                                        labelColor: Colors.teal,
+                                        indicator: BoxDecoration(),
+                                        unselectedLabelColor: Colors.black,
+                                        tabs: [
+                                          Tab(
+                                            text: 'About',
+                                          ),
+                                          Tab(text: 'Services'),
+                                          Tab(text: 'My Work'),
+                                          Tab(text: 'Blog'),
+                                        ],
                                       ),
-                                      const SizedBox(height: 20),
-                                      // Text and Buttons
-                                      buildTextColumn(),
+                                      const SizedBox(
+                                        width: 50,
+                                      ),
+                                      ElevatedButton(
+                                        onPressed: () {},
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor: Colors.white,
+                                          foregroundColor: Colors.black,
+                                          shape: const StadiumBorder(),
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 20, vertical: 10),
+                                        ),
+                                        child: const Text('Hire Me'),
+                                      ),
                                     ],
                                   ),
-                                )
-                              : Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    // Left side: Text and buttons
-                                    Expanded(
-                                      flex: 1,
-                                      child: buildTextColumn(),
-                                    ),
-                                    // Right side: Image
-                                    Expanded(
-                                      flex: 1,
-                                      child: Align(
-                                        alignment: Alignment.centerRight,
-                                        child: Image.asset(
-                                            'assets/images/developer.png'),
-                                      ),
-                                    ),
-                                  ],
-                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 20),
+                            // TabBarView
+                            Expanded(
+                              child: TabBarView(
+                                children: [
+                                  buildAboutTab(isSmallScreen),
+                                  buildServicesTab(),
+                                  buildMyWorkTab(),
+                                  buildBlogTab(),
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  );
-                },
+                      );
+                    },
+                  ),
+                ),
               ),
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget buildAboutTab(bool isSmallScreen) {
+    return isSmallScreen
+        ? SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Developer Image
+                Image.asset(
+                  'assets/images/developer.png',
+                  height: 250,
+                ),
+                const SizedBox(height: 20),
+                // Text and Buttons
+                buildTextColumn(),
+              ],
+            ),
+          )
+        : Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // Left side: Text and buttons
+              Expanded(
+                flex: 1,
+                child: buildTextColumn(),
+              ),
+              // Right side: Image
+              Expanded(
+                flex: 1,
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Image.asset('assets/images/developer.png'),
+                ),
+              ),
+            ],
+          );
+  }
+
+  Widget buildServicesTab() {
+    return const Center(
+      child: Text(
+        'Services',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Widget buildMyWorkTab() {
+    return const Center(
+      child: Text(
+        'My Work',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Widget buildBlogTab() {
+    return const Center(
+      child: Text(
+        'Blog',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
       ),
     );
   }
@@ -185,6 +224,8 @@ class HomePage extends StatelessWidget {
           style: ElevatedButton.styleFrom(
             backgroundColor: Colors.black,
             foregroundColor: Colors.white,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
             padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
           ),
           child: const Text('See My Work'),
